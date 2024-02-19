c794db43c9c93bbd1e0e9ea283ad70647b7a79bb
Chen Steenvoorden <dutchen18@gmail.com>
2024-02-19 10:34:18 +0100

    Cleanup and comments (part 15)

--- src/parser/library.ts
+++ src/parser/library.ts
@@ -1,5 +1,3 @@
-// TODO: comment
-
 import { Parser } from "./parser.js";
 import { Node } from "./node.js";
 import { Namespace } from "../declaration/namespace.js";
@@ -12,6 +10,9 @@ import { parseVariable } from "./variable.js";
 
 const EMPTY = new Generics;
 
+// Get a list of "global" classes. Global classes are classes that represent
+// the global scope in some context. For example, `Window`, or
+// `WorkerGlobalScope`.
 function getGlobalClasses(parser: Parser): ReadonlyArray<Class> {
 	const globalClasses = [];
 	globalClasses.push(parser.getRootClass("Window"));
@@ -19,7 +20,18 @@ function getGlobalClasses(parser: Parser): ReadonlyArray<Class> {
 	return globalClasses.filter((globalClass): globalClass is Class => !!globalClass);
 }
 
+// `parseLibrary` is the main entry point for converting the tree structure
+// described in "src/parser/node.ts" into an AST of C++ declarations. This
+// function recursively calls itself for namespace nodes (nodes that do not
+// have any other declaration). For non-namespace nodes, it calls any of the
+// following functions:
+// - classes: `parseClass` in "src/parser/class.ts"
+// - functions: `parseFunction` in "src/parser/function.ts"
+// - variables: `parseVariable` in "src/parser/variable.ts"
+// - type aliases: `parseTypeAlias` in "src/parser/typeAlias.ts"
 export function parseLibrary(parser: Parser, node: Node, parent?: Namespace): void {
+	// `incrementTarget` and `incrementProgress` provide some very primitive
+	// progress information that is output only in verbose mode.
 	parser.incrementTarget(node.getSize());
 
 	for (const child of node.getChildren()) {
@@ -28,15 +40,22 @@ export function parseLibrary(parser: Parser, node: Node, parent?: Namespace): vo
 		const functionDeclarations = child.getFunctionDeclarations();
 
 		if (child.basicClass) {
+			// Classes are parsed using `parseClass`.
 			parseClass(parser, child, child.basicClass, EMPTY, parent);
 
 			if (child.genericClass) {
 				parseClass(parser, child, child.genericClass, EMPTY, parent);
 			}
 		} else if (functionDeclarations.length > 0 && child.getSize() === 0) {
+			// Functions are parsed using `parseFunction`.
 			for (const declaration of functionDeclarations) {
 				parseFunction(parser, declaration, EMPTY, false, parent);
 
+				// If this is a global function, we also add it to "global"
+				// classes. For example, the `eval` function is only declared
+				// as a global function, but should still be possible to call
+				// `window.eval`. So we need functions like `eval` to be added
+				// to the `Window` class.
 				if (parent === parser.getRootNamespace()) {
 					for (const globalClass of getGlobalClasses(parser)) {
 						parseFunction(parser, declaration, EMPTY, false, globalClass);
@@ -44,18 +63,37 @@ export function parseLibrary(parser: Parser, node: Node, parent?: Namespace): vo
 				}
 			}
 		} else if (child.variableDeclaration) {
+			// Variables are parsed using `parseVariable`.
 			parseVariable(parser, child.variableDeclaration, EMPTY, parent);
 
+			// The variable node might also have children, this matches the
+			// following typescript declaration:
+			// ```
+			// declare var Foo: FooConstructor;
+			//
+			// declare namespace Foo {
+			//     function func(): void;
+			// }
+			// ```
+			//
+			// The variable is generated as normal, and we also generate a
+			// namespace, with an extra "_" at the end of its name. Because of
+			// the extra "_", there is currently no way to actually use this
+			// namespace.
+			//
+			// TODO: add an interface name for the namespace
 			if (child.getSize() > 0) {
 				parseLibrary(parser, child, new Namespace(`${child.getName()}_`, parent));
 			}
 		} else if (child.basicTypeAlias && child.typeAliasDeclaration) {
+			// Type aliases are parsed using `parseTypeAlias`.
 			parseTypeAlias(parser, child.typeAliasDeclaration, child.basicTypeAlias, EMPTY, parent);
 
 			if (child.genericTypeAlias) {
 				parseTypeAlias(parser, child.typeAliasDeclaration, child.genericTypeAlias, EMPTY, parent);
 			}
 		} else {
+			// No declarations for this node, this must be a namespace node.
 			parseLibrary(parser, child, new Namespace(child.getName(), parent));
 		}
 	}
--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -4,9 +4,8 @@ import { Class, Visibility } from "../declaration/class.js";
 import { Function } from "../declaration/function.js";
 import { Library } from "../library.js";
 import { Type } from "../type/type.js";
-import { NamedType, ANY_TYPE } from "../type/namedType.js";
+import { NamedType } from "../type/namedType.js";
 import { DeclaredType } from "../type/declaredType.js";
-import { TemplateType } from "../type/templateType.js";
 import { TypeInfo } from "./typeInfo.js";
 import { withTimer, options } from "../utility.js";
 import { addExtensions } from "../extensions.js";
@@ -16,16 +15,40 @@ import { Generics } from "./generics.js";
 import { parseLibrary } from "./library.js";
 import * as ts from "typescript";
 
+// `Parser` is the big class that stores all the information about a program
+// as it is being parsed. This class is only used to store information and
+// state of the parser, and the functions only get or update this state. This
+// class does no parsing by itself.
 export class Parser {
+	// The typescript program.
 	private readonly program: ts.Program;
+
+	// Typescript TypeChecker instance from the above program.
 	private readonly typeChecker: ts.TypeChecker;
+
+	// All parsed declarations are stored in this `Library` instance.
 	private readonly library: Library;
+
+	// The root node where yet unparsed typescript declarations are stored.
+	// See "src/parser/node.ts" for more info.
 	private readonly root: Node = new Node;
+
+	// The root namespace.
 	private readonly namespace?: Namespace;
+
+	// A map of basic and generic declared types, used by `TypeParser` to look
+	// up type information when parsing, for example, function return types.
 	private readonly basicDeclaredTypes: Map<ts.Type, DeclaredType> = new Map;
 	private readonly genericDeclaredTypes: Map<ts.Type, DeclaredType> = new Map;
+
+	// Lists of all class and function declarations. These are used after the
+	// main parsing is done to run secondary passes on all classes and
+	// functions. For example, to mark virtual base classes, or to remove
+	// duplicate declarations.
 	private readonly classes: Array<Class> = new Array;
 	private readonly functions: Array<Function> = new Array;
+
+	// Primitive progress counters.
 	private target: number = 0;
 	private progress: number = 0;
 
@@ -39,10 +62,14 @@ export class Parser {
 	}
 
 	public run(defaultLib: boolean): void {
+		// 1. Populate the `Node` tree `root` with typescript declarations. See
+		// "src/parser/node.ts" for a detailed description of why and how.
 		withTimer("discover", () => {
 			this.program.getSourceFiles().forEach(sourceFile => this.root.discover(this, sourceFile));
 		});
 
+		// 2. Convert the typescript declarations in the `Node` tree into C++
+		// declarations, the resulting declarations are stored in `library`.
 		withTimer("generate", () => {
 			if (options.namespace) {
 				parseLibrary(this, this.root, new Namespace(options.namespace, this.namespace));
@@ -51,23 +78,28 @@ export class Parser {
 			}
 		});
 
+		// 3. Remove duplicate declarations.
 		withTimer("remove duplicates", () => {
 			this.library.removeDuplicates();
 			this.classes.forEach(declaration => declaration.removeDuplicates());
 		});
 
+		// 4. Add extensions, if this is the default library.
 		if (defaultLib) {
 			addExtensions(this);
 		}
 
+		// 5. Compute virtual base classes.
 		withTimer("compute virtual base classes", () => {
 			this.classes.forEach(declaration => declaration.computeVirtualBaseClasses());
 		});
 
+		// 6. Add `using` declarations for base members that require them.
 		withTimer("use base members", () => {
 			this.classes.forEach(declaration => declaration.useBaseMembers());
 		});
 
+		// 7. Add `cheerp::client_layout` attribute to the `Object` class.
 		const objectClass = this.getRootClass("Object");
 
 		if (objectClass) {
@@ -107,14 +139,23 @@ export class Parser {
 		return this.namespace;
 	}
 
+	// A quick and dirty way to get the non-generic version of a class in
+	// global scope. This is mostly used in "src/extensions.ts", to modify
+	// specific classes that are known to exist.
 	public getRootClass(name: string): Class | undefined {
 		return this.root.getChild(name)?.basicClass;
 	}
 
+	// A quick and dirty way to get the generic version of a class in
+	// global scope. This is mostly used in "src/extensions.ts", to modify
+	// specific classes that are known to exist.
 	public getGenericRootClass(name: string): Class | undefined {
 		return this.root.getChild(name)?.genericClass;
 	}
 
+	// A quick and dirty way to get a non-generic type in global scope. This is
+	// used in "src/extensions.ts" and "src/parser/typeParser.ts" to obtain a
+	// reference to a specific class type that is known to exist.
 	public getRootType(name: string): Type {
 		const declaration = this.getRootClass(name);
 
@@ -125,6 +166,9 @@ export class Parser {
 		}
 	}
 
+	// A quick and dirty way to get a generic type in global scope. This is
+	// used in "src/extensions.ts" and "src/parser/typeParser.ts" to obtain a
+	// reference to a specific class type that is known to exist.
 	public getGenericRootType(name: string): Type {
 		const declaration = this.getGenericRootClass(name);
 
@@ -151,6 +195,9 @@ export class Parser {
 		return this.genericDeclaredTypes.get(type);
 	}
 
+	// Check if a typescript node is to be included in the output file. This
+	// check is used to prune declarations from files that are included
+	// indirectly but whose declarations we do not want to emit.
 	public includesDeclaration(node: ts.Node): boolean {
 		return this.library.hasFile(node.getSourceFile().fileName);
 	}
@@ -174,6 +221,7 @@ export class Parser {
 	public incrementProgress(child: Child): void {
 		this.progress += 1;
 
+		// Output some progress information in verbose mode.
 		if (options.isVerboseProgress) {
 			console.log(`${this.progress}/${this.target} ${child.getName()}`);
 		}
@@ -187,6 +235,20 @@ export class Parser {
 		}
 	}
 
+	// Instead of manually adding a declaration to the library or parent class,
+	// `addDeclaration` should be called for every declaration. This function
+	// does a few things:
+	// - Update the parent of the declaration.
+	// - If the parent is a class, add the declaration as a member of the class
+	//   with public visibility.
+	// - If the parent is a namespace, or if the declaration is itself a class,
+	//   add the global declaration to the *library*. Inner classes are also
+	//   added as global declarations so their complete definitions may appear
+	//   outside of their parent class, eg. as `class Foo::Bar {};`.
+	// - If the declaration is a class in global scope, compute its references.
+	//   Computing the references of the global class will also recursive
+	//   compute references of all inner classes inside of it.
+	// - Register the declaration using `registerDeclaration`.
 	public addDeclaration(declaration: Declaration, parent?: Namespace): void {
 		declaration.setParent(parent);
 
